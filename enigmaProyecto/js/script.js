46b5164e5960c512212877897e96d1e83de9676f
paopaniar <98674071+paopaniar@users.noreply.github.com>
2022-04-14 21:55:58 -0600

    Add files via upload

--- enigmaProyecto/js/script.js
+++ enigmaProyecto/js/script.js
@@ -1,13 +1,13 @@
 $.ajax('https://tipodecambio.paginasweb.cr/api', 
 {
+    type: "GET",    
     dataType: 'json', // type of response data
     timeout: 500,     // timeout milliseconds
-    success: function (data,status,xhr) {   // success callback function
-        $('#compra').append(data.compra);
-        $('#venta').append(data.venta);
-        $('#fecha').append(data.fecha);
-        
-    },
+    success: function (data) {
+    $('#compra').append(data.compra);
+    $('#venta').append(data.venta);
+    $('#fecha').append(data.fecha);
+},
     error: function (jqXhr, textStatus, errorMessage) { // error callback 
       $('#compra').append('Error: ' + errorMessage);
       $('#venta').append('Error: ' + errorMessage);
